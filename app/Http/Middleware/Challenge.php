1bc7bc9780fba647f9dc1dcd9d2ac36d1997baad
ck <2314@qq.com>
2024-06-17 14:31:32 +0800

    减少人机验证时间

--- app/Http/Middleware/Challenge.php
+++ app/Http/Middleware/Challenge.php
@@ -8,6 +8,9 @@ use GeoIp2\Database\Reader;
 
 class Challenge
 {
+    private $whiteClass = [
+        "App\Http\Controllers\Pay"
+        ];
     /**
      * Handle an incoming request.
      *
@@ -17,32 +20,36 @@ class Challenge
      */
     public function handle($request, Closure $next)
     {
+
+        if(in_array($request->route()->getAction()["namespace"],$this->whiteClass))
+            return $next($request);
+
         if(dujiaoka_config_get('is_cn_challenge') == BaseModel::STATUS_CLOSE)
             return $next($request);
-            
+
         $status = session('challenge');
         if($status === "pass")
             return $next($request);
 
         if(isset($_REQUEST['_challenge'])){
-            if (substr(sha1($_REQUEST['_challenge']), -4) == $status){
+            if (substr(sha1($_REQUEST['_challenge']), -2) == $status){
                 session(['challenge' => 'pass']);
                 return $next($request);
             }
         }
-        
+
         // 对Cloudflare站点的支持优化
-        if(isset($_SERVER["HTTP_CF_IPCOUNTRY"]))
-            $isoCode = $_SERVER["HTTP_CF_IPCOUNTRY"];
-        else{
-            $reader = new Reader(storage_path('app/library/GeoLite2-Country.mmdb'));
-            $isoCode = $reader->country($request->ip())->country->isoCode;
-        }
-        if($isoCode != 'CN'){
-            session(['challenge' => 'pass']);
-            return $next($request);
-        }
-        $challenge = substr(sha1(rand()), -4);
+//         if(isset($_SERVER["HTTP_CF_IPCOUNTRY"]))
+//             $isoCode = $_SERVER["HTTP_CF_IPCOUNTRY"];
+//         else{
+//             $reader = new Reader(storage_path('app/library/GeoLite2-Country.mmdb'));
+//             $isoCode = $reader->country($request->ip())->country->isoCode;
+//         }
+//         if($isoCode != 'CN'){
+//             session(['challenge' => 'pass']);
+//             return $next($request);
+//         }
+        $challenge = substr(sha1(rand()), -2);
         session(['challenge' => $challenge]);
         return response()->view('common/challenge',['code' => $challenge]);
     }
